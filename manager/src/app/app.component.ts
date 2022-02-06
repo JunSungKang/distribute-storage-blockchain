e1020bcb79e6740b279530bda6e7c96f7602a810
jskang <kjuns1@naver.com>
2022-02-07 01:59:08 +0900

    Feat: File upload window and uploaded file list UI creation.

--- manager/src/app/app.component.ts
+++ manager/src/app/app.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
 
+export interface Section {
+  name: string;
+  updated: Date;
+  damaged: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'manager';
+  fileName: string = "";
+  folders: Section[] = [
+    {
+      name: 'Photos',
+      updated: new Date('1/1/16'),
+      damaged: false,
+    },
+    {
+      name: 'Recipes',
+      updated: new Date('1/17/16'),
+      damaged: false,
+    },
+    {
+      name: 'Work',
+      updated: new Date('1/28/16'),
+      damaged: false,
+    },
+  ];
+
+  onFileSelected = (event: any) => {
+
+    const file:File = event.target.files[0];
+
+    if (file) {
+      this.fileName = file.name;
+      const formData = new FormData();
+      formData.append("thumbnail", file);
+      console.log(formData);
+    }
+  }
+
+  fileDownload = (folder: Section) => {
+    alert(folder.name);
+  }
 }
--- manager/src/app/app.module.ts
+++ manager/src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    MatListModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
