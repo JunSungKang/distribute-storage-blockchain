cfe2bfd9640f470081558af24a0badd4027e3ffb
jskang <kjuns1@naver.com>
2022-02-08 01:58:41 +0900

    chore(log): Argument option add. and log format change.

--- connector/src/main/java/com/jskang/storageclient/Main.java
+++ connector/src/main/java/com/jskang/storageclient/Main.java
@@ -2,11 +2,14 @@ package com.jskang.storageclient;
 
 import com.jskang.storageclient.file.Download;
 import com.jskang.storageclient.file.Upload;
-import com.jskang.storageclient.network.NodeInfo;
 import com.jskang.storageclient.reedsolomon.ReedSolomonDecoding;
 import java.io.File;
 import java.io.IOException;
-import java.util.Scanner;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,49 +17,47 @@ public class Main {
 
     private static Logger LOG = LoggerFactory.getLogger(Main.class);
 
-    public static void viewUsing() {
-        System.out.println("===================================");
-        System.out.println("input 'help' : help.");
-        System.out.println("input 'exit' : program exit.");
-        System.out.println("input 'upload' : upload.");
-        System.out.println("input 'download' : download.");
-        System.out.println("===================================");
-    }
+    public static void main(String[] args) throws IOException, ParseException {
+        // Create options object
+        Options options = new Options();
+        options.addOption("t", "type", true, "upload | download");
+        options.addOption("f", "file", true, "file path");
 
-    public static void main(String[] args) throws IOException {
-        NodeInfo nodeInfo = new NodeInfo();
-        nodeInfo.load();
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse(options, args);
 
-        LOG.info(nodeInfo.getNodeList().toString());
+        // Select type option Upload or Download.
+        String type = "";
+        if (!cmd.hasOption("t")) {
+            LOG.error("The type(-t) option is missing.");
+            return;
+        }
+        type = cmd.getOptionValue("t");
+
+        // Select type option Upload or Download.
+        String path = "";
+        if (!cmd.hasOption("f")) {
+            LOG.error("The file(-f) option is missing.");
+            return;
+        }
+        path = cmd.getOptionValue("f");
 
         LOG.info("Client start completed.");
-        viewUsing();
 
-        String inputText = "";
-        while (!inputText.equals("exit")) {
-            Scanner scanner = new Scanner(System.in);
-            inputText = scanner.next();
+        if (type.equals("upload")) {
+            LOG.info("Please enter the file path to upload.");
 
-            switch (inputText) {
-                case "help":
-                    viewUsing();
-                    break;
-                case "upload":
-                    LOG.info("Please enter the file path to upload.");
+            File file = new File(path);
+            LOG.info(new Upload().excute(file.getAbsolutePath()));
+        } else if (type.equals("download")) {
+            LOG.info("Please enter the file path to download.");
 
-                    File file = new File(scanner.next());
-                    LOG.info(new Upload().excute(file.getAbsolutePath()));
-                    break;
-                case "download":
-                    LOG.info("Please enter the file path to download.");
-
-                    String downloadFile = scanner.next();
-                    new Download().excute(downloadFile);
-                    ReedSolomonDecoding reedSolomonDecoding = new ReedSolomonDecoding();
-                    reedSolomonDecoding.execute("test_merge\\" + downloadFile);
-                    break;
-            }
+            String downloadFile = path;
+            new Download().excute(downloadFile);
+            ReedSolomonDecoding reedSolomonDecoding = new ReedSolomonDecoding();
+            reedSolomonDecoding.execute("test_merge\\" + downloadFile);
+        } else {
+            LOG.error("The type(-t) option is unknown.");
         }
-
     }
 }
